833b6b5820994cedda9868a74a627c8a2b18ca1b
ranajaydas <ranajay@outlook.com>
2019-09-07 11:34:01 +0300

    It's the beer song!

---
+++ 1_basics_beersong.py
@@ -0,0 +1,16 @@
+""" It's the '99 bottles of beer' song y'all! """
+bottle_word = 'bottles'
+
+for beer_num in range(99, 0, -1):
+    print('{} {} of beer on the wall,'.format(beer_num, bottle_word))
+    print('{} {} of beer,'.format(beer_num, bottle_word))
+    print('Take one down and pass it around,')
+
+    if beer_num == 1:
+        print('No more bottles of beer on the wall!')
+    else:
+        new_num = beer_num-1
+        if new_num == 1:
+            bottle_word = 'bottle'
+        print('{} {} of beer on the wall!'.format(new_num, bottle_word))
+    print()
