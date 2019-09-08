90bcf3575f393ea653b1c8583f5d0cb0b8d137c9
ranajaydas <ranajay@outlook.com>
2019-09-08 12:44:15 +0300

    Added comments

--- ch04_func_lettersearch.py
+++ ch04_func_lettersearch.py
@@ -1,3 +1,6 @@
+# Copy of this function in the 'ch04_func_installing_into_sitepackages' folder
+
+
 def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     """Return a set of 'letters' found in 'phrase'."""
     return set(letters.lower()).intersection(set(phrase.lower()))
