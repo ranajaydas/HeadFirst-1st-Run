efd74381c671798e6cd9c3774a526ce57e09d380
ranajaydas <ranajay@outlook.com>
2019-09-08 10:24:03 +0300

    Removed test code from end of the file

--- ch04_func_lettersearch.py
+++ ch04_func_lettersearch.py
@@ -3,9 +3,4 @@ def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     return set(letters).intersection(set(phrase))
 
 
-print(search4letters("Hitch-hiker's Guide to the Galaxy", "sky"))
-print(search4letters("Ranajay Das is awesome", "pp"))
-print(search4letters("Choot pakoda"))
-print(help(search4letters))
-
 
