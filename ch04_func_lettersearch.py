f7f1773f26c2769242c6af872eedecc34667f432
ranajaydas <ranajay@outlook.com>
2019-09-08 11:10:08 +0300

    Changed 'letters' and 'phrase' to lower case

--- ch04_func_lettersearch.py
+++ ch04_func_lettersearch.py
@@ -1,3 +1,3 @@
 def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     """Return a set of 'letters' found in 'phrase'."""
-    return set(letters).intersection(set(phrase))
+    return set(letters.lower()).intersection(set(phrase.lower()))
